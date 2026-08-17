748ea1caecdd26c0eb8461bd6dd3f497503227c8
Luis Javier Chavez <ljchavez@unicesar.edu.co>
2026-08-17 17:20:05 -0500

    feat: crear clase Persona como padre de Cliente

---
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author luisj
+ */
+public class Person {
+    protected String  id;
+    protected String  person;
+}
